448b4a99670c614c95679cb1e00273d06080677c
DevSquadMember <alexis.dufrenne@hotmail.fr>
2015-04-14 16:54:58 +0900

    Showing a simple menu screen

--- core/src/com/color/game/ColorGame.java
+++ core/src/com/color/game/ColorGame.java
@@ -2,10 +2,11 @@ package com.color.game;
 
 import com.badlogic.gdx.Game;
 import com.color.game.screens.GameScreen;
+import com.color.game.screens.MenuScreen;
 
 public class ColorGame extends Game {
 	@Override
 	public void create () {
-        this.setScreen(new GameScreen());
+        this.setScreen(new MenuScreen(this));
 	}
 }
---
+++ core/src/com/color/game/screens/MenuScreen.java
@@ -0,0 +1,112 @@
+package com.color.game.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+
+/**
+ * Created by sowipheur on 14/04/15.
+ */
+public class MenuScreen implements Screen {
+
+    Game game;
+    Stage stage;
+    Skin skin;
+    TextButton textButton;
+
+    public MenuScreen(final Game game) {
+        this.game = game;
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+        // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+        skin = new Skin();
+        // Generate a 1x1 white texture and store it in the skin named "white".
+        Pixmap pixmap = new Pixmap(100, 100, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.BLACK);
+        pixmap.fill();
+
+        skin.add("white", new Texture(pixmap));
+
+        // Store the default libgdx font under the name "default".
+        BitmapFont bfont=new BitmapFont();
+        bfont.scale(1);
+        skin.add("default",bfont);
+
+        // Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.RED);
+        textButtonStyle.down = skin.newDrawable("white", Color.GREEN);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.WHITE);
+
+        textButtonStyle.font = skin.getFont("default");
+
+        skin.add("default", textButtonStyle);
+
+        // Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+        textButton = new TextButton("PLAY", textButtonStyle);
+        textButton.setPosition(300 - textButton.getMinWidth()/2, 150 - textButton.getMinHeight()/2);
+        stage.addActor(textButton);
+        stage.addActor(textButton);
+        stage.addActor(textButton);
+
+        textButton.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                game.setScreen( new GameScreen());
+            }
+        });
+    }
+
+    @Override
+    public void show() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
